ccbeb344e0880dcfc7c106597d7a091aec0b67c5
Ytherambugua <mbugualinnete@gmail.com>
2023-12-18 10:28:20 -0800

    2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,7 +3,7 @@ def safe_print_list_integers(my_list=[], x=0):
     count = 0
     try:
         for i in range(x):
-    value = int(my_list[i])
+            value = int(my_list[i])
     print("{:d}".format(value), end=" ")
         count += 1
     print ()
