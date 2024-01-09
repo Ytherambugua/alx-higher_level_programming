44d8a8a1a8d7c1b843252fdd6c4bf6aafe31655d
Ytherambugua <mbugualinnete@gmail.com>
2024-01-09 10:42:22 +0000

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
+""" Module that contains a function that reads from a file """
+
+
 def read_file(filename=""):
-    with open('UTF8.txt', 'w') as file:
-        content = file.read()
-        print(content) 
+    """ Function that reads from a file
+
+    Args:
+        filename: filename
+
+    Raises
+        Exception: when the file can be opened
+
+    """
+
+    with open(filename, 'r', encoding="utf-8") as f:
+        read_data = f.read()
+        print(read_data, end='')
