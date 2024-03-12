5b1152ad0050471759d9d53043732867731e9a35
Ytherambugua <mbugualinnete@gmail.com>
2024-03-12 10:52:36 -0700

    JavaScript

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-//Using const to declare constants
+// Using const to declare constants
 const line1 = "C is fun";
 const line2 = "Python is cool";
 const line3 = "JavaScript is amazing";
-//Printing each line with console.log
+// Printing each line with console.log
 console.log(line1);
 console.log(line2);
 console.log(line3);
