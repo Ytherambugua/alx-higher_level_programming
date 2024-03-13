46dc8ffa97b796f5dd0e6b056968883febde4890
Ytherambugua <mbugualinnete@gmail.com>
2024-03-13 12:34:30 -0700

    JavaScript

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const argNum = process.argv[2];
+
+if (isNaN(argNum)) {
+	  console.log('Missing size');
+} else {
+	for (let i = 0; i < argNum; i++) {
+		let square = '';
+		for (let j = 0; j < argNum; j++) {
+			square = square + 'X';
+		}
+		console.log(square);
+	}
+}
