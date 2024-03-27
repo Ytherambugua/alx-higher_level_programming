517d004eba32072620d1c36d33fd596e044b41f2
Ytherambugua <mbugualinnete@gmail.com>
2024-03-27 05:46:29 -0700

    Python Network

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #A script that takes in a URL, and sends POST to the passed URL nad displays the body of the response
-curl -X POST -d "email=$email$subject=$subject" -s "$1"
+curl -X POST -d "email=test@gmail.com=I will always be here for PLD"
